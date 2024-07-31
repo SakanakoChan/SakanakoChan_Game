7fded407ad75f1c78d109eae362bd3d765fe6411
SakanakoChan <148366895+SakanakoChan@users.noreply.github.com>
2024-07-31 15:55:30 +0800

    Day 18 Added UI Header

---
+++ Assets/Scripts/UI/UI.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UI : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SwitchToMenu(GameObject _menu)
+    {
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            transform.GetChild(i).gameObject.SetActive(false);
+        }
+
+        if (_menu != null)
+        {
+            _menu.SetActive(true);
+        }
+    }
+}
