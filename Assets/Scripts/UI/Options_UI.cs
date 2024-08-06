e717c9e6156fe124a1ad87fd549fce04a09f901e
SakanakoChan <148366895+SakanakoChan@users.noreply.github.com>
2024-08-06 23:43:30 +0800

    Day 24 Finalized, Added Options UI

---
+++ Assets/Scripts/UI/Options_UI.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Options_UI : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
