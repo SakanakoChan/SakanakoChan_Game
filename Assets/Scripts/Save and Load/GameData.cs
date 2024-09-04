6d13a6da6a372977f1a94b5a9f354ae71b8d9d29
SakanakoChan <148366895+SakanakoChan@users.noreply.github.com>
2024-09-04 23:23:42 +0800

    some code cleanups

--- Assets/Scripts/Save and Load/GameData.cs
+++ Assets/Scripts/Save and Load/GameData.cs
@@ -22,19 +22,6 @@ public class GameData
     [Header("Map Elements")]
     public List<int> UsedMapElementIDList;
 
-    //[Header("Audio settings")]
-    //public SerializableDictionary<string, float> volumeSettingsDictionary;
-
-    //[Header("Keybind Settings")]
-    //public SerializableDictionary<string, KeyCode> keybindsDictionary;
-    ////public SerializableDictionary<string, KeyCode> keybindsDictionary_Chinese; 
-
-    //[Header("Gameplay Settings")]
-    //public SerializableDictionary<string, bool> gameplayToggleSettingsDictionary;
-
-    //[Header("Language Settings")]
-    //public int localeID; //0 for english, 1 for chinese
-
     public GameData()
     {
         //Game Progression
@@ -58,50 +45,5 @@ public class GameData
 
         closestActivatedCheckpointID = string.Empty;
         lastActivatedCheckpointID = string.Empty;
-
-        //Settings
-
-        //default language is english
-        //localeID = 0;
-
-        ////volumeSettingsDictionary<exposedParameter, value>
-        //volumeSettingsDictionary = new SerializableDictionary<string, float>();
-
-        //keybindsDictionary = new SerializableDictionary<string, KeyCode>();
-        //SetupDefaultKeybinds();
-
-        ////keybindsDictionary_Chinese = new SerializableDictionary<string, KeyCode>();
-        ////SetupDefaultKeybinds_Chinese();
-
-        ////gameplay toggle settings
-        //gameplayToggleSettingsDictionary = new SerializableDictionary<string, bool>();
     }
-
-    //private void SetupDefaultKeybinds()
-    //{
-    //    keybindsDictionary.Add("Attack", KeyCode.Mouse0);
-    //    keybindsDictionary.Add("Aim", KeyCode.Mouse1);
-    //    keybindsDictionary.Add("Flask", KeyCode.Alpha1);
-    //    keybindsDictionary.Add("Dash", KeyCode.LeftShift);
-    //    keybindsDictionary.Add("Parry", KeyCode.Q);
-    //    keybindsDictionary.Add("Crystal", KeyCode.F);
-    //    keybindsDictionary.Add("Blackhole", KeyCode.R);
-    //    keybindsDictionary.Add("Character", KeyCode.C);
-    //    keybindsDictionary.Add("Craft", KeyCode.B);
-    //    keybindsDictionary.Add("Skill", KeyCode.K);
-    //}
-
-    //private void SetupDefaultKeybinds_Chinese()
-    //{
-    //    keybindsDictionary.Add("攻击", KeyCode.Mouse0);
-    //    keybindsDictionary.Add("瞄准", KeyCode.Mouse1);
-    //    keybindsDictionary.Add("元素瓶", KeyCode.Alpha1);
-    //    keybindsDictionary.Add("冲刺", KeyCode.LeftShift);
-    //    keybindsDictionary.Add("弹反", KeyCode.Q);
-    //    keybindsDictionary.Add("水晶", KeyCode.F);
-    //    keybindsDictionary.Add("黑洞", KeyCode.R);
-    //    keybindsDictionary.Add("角色面板", KeyCode.C);
-    //    keybindsDictionary.Add("制造面板", KeyCode.B);
-    //    keybindsDictionary.Add("技能面板", KeyCode.K);
-    //}
 }
